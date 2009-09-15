5a39ca5c029025b98d5eef4bd6298d3ea59a8f32
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-09-15 11:59:24 +0900

    initial commit

---
+++ lib/FCGI/Client.pm
@@ -0,0 +1,33 @@
+package FCGI::Client;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+FCGI::Client -
+
+=head1 SYNOPSIS
+
+  use FCGI::Client;
+
+=head1 DESCRIPTION
+
+FCGI::Client is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom @*(#RJKLFHFSDLJF gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
